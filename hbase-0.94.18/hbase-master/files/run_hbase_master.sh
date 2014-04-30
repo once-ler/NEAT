bea12de511ea89caea3a880d8eb8546173d19722
htaox <htaox@hotmail.com>
2014-04-30 10:27:17 -0400

    Pass $IP variable to run_hbase_master.sh

--- hbase-0.94.18/hbase-master/files/run_hbase_master.sh
+++ hbase-0.94.18/hbase-master/files/run_hbase_master.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-#/opt/spark-0.9.0/sbin/start-master.sh
 
+rm "${HBASE_HOME}/logs"/* 
+
+echo -n "starting zookeeper"
 "${HBASE_HOME}/bin"/hbase-daemons.sh --config "${HBASE_CONF_DIR}" start zookeeper
-"${HBASE_HOME}/bin"/hbase-daemon.sh --config "${HBASE_CONF_DIR}" start master 
-
+#Simple test to see if zookeeper started
 while [ 1 ];
 do
-	tail -f /opt/hbase-${HBASE_VERSION}/logs/*.out
+	if [ -f "${HBASE_HOME}/logs"/*zookeeper*.log ]
+    then    
+        break
+    else
+        sleep 1
+    fi
+done
+
+echo -n "starting HBase master"
+"${HBASE_HOME}/bin"/hbase-daemon.sh --config "${HBASE_CONF_DIR}" start master 
+while [ 1 ];
+do
+	tail -f "${HBASE_HOME}/logs"/*.out
         sleep 1
 done
