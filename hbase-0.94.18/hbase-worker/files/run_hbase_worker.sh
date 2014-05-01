c96daea2e327cb0ccf1b2b4d93baaf18a5ce9d7b
htaox <htaox@hotmail.com>
2014-05-01 16:16:18 -0400

    Updated worker startup routine

--- hbase-0.94.18/hbase-worker/files/run_hbase_worker.sh
+++ hbase-0.94.18/hbase-worker/files/run_hbase_worker.sh
@@ -3,3 +3,8 @@
 # ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER
 
 "${HBASE_HOME}/bin"/hbase-daemons.sh --config "${HBASE_CONF_DIR}" start regionserver
+while [ 1 ];
+do
+	tail -f "${HBASE_HOME}/logs"/*.out
+        sleep 1
+done
