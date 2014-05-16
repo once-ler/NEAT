d339e0e9a5127bcb7c1ac18dcff370486dd32d3c
htaox <htaox@hotmail.com>
2014-05-16 13:19:32 -0400

    Use ipv4 for Zookeeper & HBase

--- hbase-0.94.18/hbase-base/files/hbase-env.sh
+++ hbase-0.94.18/hbase-base/files/hbase-env.sh
@@ -38,7 +38,7 @@
 # Below are what we set by default.  May only work with SUN JVM.
 # For more on why as well as other possible settings,
 # see http://wiki.apache.org/hadoop/PerformanceTuning
-export HBASE_OPTS="-XX:+UseConcMarkSweepGC"
+export HBASE_OPTS="-XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true"
 
 # Uncomment one of the below three options to enable java garbage collection logging for the server-side processes.
 
